5b0127b0418e7dd5eaa1a007e69cf79d9f5d8cdb
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2026-02-03 02:42:19 +0200

    feat: added redux

--- expo-app/app/_layout.tsx
+++ expo-app/app/_layout.tsx
@@ -1,15 +1,19 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 function RootLayout() {
   return (
-    <View style={{ flex: 1, alignSelf: "stretch" }}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={{ flex: 1, alignSelf: "stretch" }}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
