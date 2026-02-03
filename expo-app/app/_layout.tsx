b0efc6091a4b90e92d6d8c58a68ce4643ce25a57
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2026-02-03 02:05:05 +0200

    feat: expo app: Layout: stack

--- expo-app/app/_layout.tsx
+++ expo-app/app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <View>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
