c22e429a33ecef9e9d00057b0c8b553a3893ed6a
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2026-02-12 16:34:49 +0200

    feat: expo: settings: added bg colors buttons

--- TODO.md
+++ TODO.md
@@ -2,10 +2,12 @@
 
 - Create a useAppSelector ✅
 - Create a useAppDispatch ✅
-- create a useColors hook, that reads from the Redux state, and gets the color right
+- create a useColors hook, that reads from the Redux state, and gets the color right ✅
 - Enable the setting tab to change the theme
 - install Async Storage (Or any new expo update)
 - Check if anything is new with redux perstent storage?
 - store the theme with Async STorage
 - init theme on app opening
 - use colors all over the app
+- App Bar
+- Tabs colors
--- expo-app/src/app/(tabs)/settings.tsx
+++ expo-app/src/app/(tabs)/settings.tsx
@@ -1,17 +1,63 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { useColors } from "@/hooks/colors";
+import React from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import type { ThemeType } from "@/redux/themeSlice/themeSlice";
+import { useAppSelector } from "@/redux/store";
 
 const Settings = () => {
   const colors = useColors();
   return (
     <SafeAreaView
-      //className="flex-1 items-center "
+      className="flex-1 self-stretch "
       style={{ backgroundColor: colors.background }}
     >
-      <Text style={{ color: colors.text }}>Settings</Text>
+      <ScrollView
+        contentContainerStyle={{ alignSelf: "stretch", flex: 1, padding: 16 }}
+      >
+        <Text
+          className=" text-[32px] font-semibold"
+          style={{ color: colors.text }}
+        >
+          Theme:
+        </Text>
+
+        <View className="gap-4 mt-4 self-stretch">
+          <ThemeSelectorCard themeType="light" text="Light" />
+          <ThemeSelectorCard themeType="dark" text="Dark" />
+          <ThemeSelectorCard themeType="system" text="System" />
+        </View>
+      </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const ThemeSelectorCard = ({
+  text,
+  themeType,
+}: {
+  themeType: ThemeType;
+  text: string;
+}) => {
+  const activeTheme = useAppSelector((state) => state.theme.value);
+
+  const isActive = activeTheme === themeType;
+
+  const colors = useColors();
+
+  return (
+    <TouchableOpacity
+      className=" self-stretch border-[1] rounded-[8px] px-3 py-4 flex-row items-center gap-4 flex-wrap"
+      style={{
+        borderColor: isActive ? colors.primary : colors.border,
+        //padding: 16,
+        borderWidth: 4,
+      }}
+    >
+      <Text style={{ color: colors.text }} className=" text-[24px]">
+        {text}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
