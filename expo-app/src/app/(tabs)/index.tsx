5d2a6bfcefdec19ecc503a4d7d3c5361c2c45402
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2026-02-12 21:34:30 +0200

    feat: expo: linked to next endpoint

--- expo-app/src/app/(tabs)/index.tsx
+++ expo-app/src/app/(tabs)/index.tsx
@@ -11,10 +11,38 @@ function App() {
   const { messages, error, sendMessage } = useChat({
     transport: new DefaultChatTransport({
       fetch: expoFetch as unknown as typeof globalThis.fetch,
-      api: generateAPIUrl("/api/chat"),
+      api: "http://10.0.2.2:3000/api/chat",
     }),
     onError: (error) => console.error(error, "ERROR"),
+    messages: [
+      {
+        parts: [
+          {
+            type: "text",
+            text: "Yo",
+          },
+        ],
+        id: "tHPAGNPsjSDJXrPd",
+        role: "user",
+      },
+      {
+        id: "q6vnyGxDxF9JBq04",
+        role: "assistant",
+        parts: [
+          {
+            type: "step-start",
+          },
+          {
+            type: "text",
+            text: "Hey there. Thanks for reaching out.\n\nI'm here to listen without judgment whenever you're ready to share what's on your mind. How can I help you today?",
+            state: "done",
+          },
+        ],
+      },
+    ],
   });
+
+  console.log(JSON.stringify(messages, null, 2));
 
   if (error) return <Text>{error.message}</Text>;
 
